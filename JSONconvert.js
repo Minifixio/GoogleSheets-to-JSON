2a0f714d40c4b5959fad36efec93cfdff90ba7d1
minifixio <emile@le-gallic.com>
2023-01-23 19:55:07 +0100

    first commit

---
+++ JSONconvert.js
@@ -0,0 +1,34 @@
+function onOpen() {
+    var ui = SpreadsheetApp.getUi();
+    ui.createMenu('JSON').addItem('Convert active spreadsheet', 'convertActiveSpreadsheet').addToUi();
+}
+
+function convertActiveSpreadsheet() {
+  var result={};
+  var tableName = SpreadsheetApp.getActiveSheet().getName();
+  var data = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1').getDataRange().getValues();
+  var dataJSON = makeJSON(data, tableName);
+  displayJSON(dataJSON);
+}
+
+function makeJSON(data, tableName) {
+  res = {};
+  keys = data[0];
+  content = [];
+  for (var i=1; i<data.length; i++) {
+    obj = {};
+    keys.forEach((key, ind) => {
+      obj[key] = data[i][ind];
+    });
+    content.push(obj);
+  }
+  res[tableName] = content;
+  return JSON.stringify(res, null, "\t");
+}
+
+function displayJSON(text) {
+  var output = HtmlService.createHtmlOutput("<textarea style='width:100%;' rows='30'>" + text + "</textarea>");
+  output.setWidth(700)
+  output.setHeight(500);
+  SpreadsheetApp.getUi().showModalDialog(output, 'JSON output');
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# Apps Script convert Google Sheets to JSON
+
+## How to use ?
+1. Fill your sheet with you desired data
+The first row indicates the propreties keys and the following rows the values, as in the example.
+
+*Example*
+![How to write your data](./screens/screen1.png)
+
+<br>
+
+2. Add the extension
+Go to **Extensions -> Apps Script** then **Add File** and copy the code from `JSONconvert.j` in it.
+Now press **Run** and return to your spreadsheet.
+
+*Add the extension*
+![Add the extension](./screens/screen2.png)
+
+<br>
+
+3. Convert your data
+Select the **JSON** menu from the menu bar and the **Convert active spreadsheet**.
+*NOTE :* The result will be an object named afer your spreadsheet name with the array of objects from you sheet.
+
+*Run the script*
+![Run the script](./screens/screen3.png)
+
+<br>
+
+Feel free to share your issues/improvements.
